7c26ebad113d29daa7f91a061b98f0e839841b0b
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-02-28 13:52:30 +0300

    Completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_nums = [num for num in num_list if num % 2 == 0]
+    return even_nums
+
+even_nums = return_evens([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+print(even_nums)
 
 def make_exclamation(sentence_list):
-    pass
+    exclamations = [sentence + "!" for sentence in sentence_list]
+    return exclamations
+
+new_sentence = make_exclamation(["Hello", "World"])
+print(new_sentence)
